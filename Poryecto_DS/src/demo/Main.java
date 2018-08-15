711d9e7155dcae9b7be8928f2111c583973f241b
Cliente CONTROL-PC <CltControl@GGIECLB3WRK069.espol.edu.ec>
2018-08-15 11:07:55 -0500

    Ventanas Prueba

--- Poryecto_DS/src/demo/Main.java
+++ Poryecto_DS/src/demo/Main.java
@@ -5,47 +5,49 @@ import model.Vendedor;
 import model.VendedorEditarVenta;
 import model.Administrador;
 import model.NotificacionPeticion;
+import view.MainView;
 
 /**
- * 
+ *
  */
 public class Main {
-        public static void main(String[] args) {
-            /*
+
+    public static void main(String[] args) {
+        /*
             Prueba decorate
-            */
-            Cliente c = new Cliente();
-            Vendedor v = new Vendedor();            
-            v.agregarCliente(c);
-            VendedorEditarVenta ve = new VendedorEditarVenta(v);
-            ve.operacion(v);    
-            
-            /*
+         */
+        Cliente c = new Cliente();
+        Vendedor v = new Vendedor();
+        v.agregarCliente(c);
+        VendedorEditarVenta ve = new VendedorEditarVenta(v);
+        ve.operacion(v);
+
+        /*
             Prueba observer
-            */
-            Administrador adm = new Administrador();
-            NotificacionPeticion np = new NotificacionPeticion();
-            np.attach(adm);            
-            v.pedirPermiso(np);                                                                   
+         */
+        Administrador adm = new Administrador();
+        NotificacionPeticion np = new NotificacionPeticion();
+        np.attach(adm);
+        v.pedirPermiso(np);
     }
-	/**
-	 * Default constructor
-	 */
-	public Main() {
-	}
+    /**
+     * Default constructor
+     */
+    public Main() {
+    }
 
-	/**
-	 * @return
-	 */
-	public void main() {
-		// TODO implement here
-	}
+    /**
+     * @return
+     */
+    public void main() {
+        // TODO implement here
+    }
 
-	/**
-	 * @return
-	 */
-	public void conectarDatabase() {
-		// TODO implement here
-	}
+    /**
+     * @return
+     */
+    public void conectarDatabase() {
+        // TODO implement here
+    }
 
 }
