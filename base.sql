6ab4b6aeee45b9afa23272b9f9357956bfc3782b
emiliomoran <emiliojmp9@gmail.com>
2018-08-24 12:34:46 -0500

    base correccion

--- base.sql
+++ base.sql
@@ -2,100 +2,109 @@ Drop database if exists lineaBlanca;
 create database lineaBlanca;
 use lineaBlanca;
 
-create table cliente(
-	
-	Cli_Cedula int AUTO_INCREMENT primary key,
-    Cli_Nombre varchar(50),
-    Cli_Direccion varchar(30),
-    Cli_Telefono varchar(10),
-    Cli_email varchar(20)
+create table t_cliente(
+    Cli_id int auto_increment primary key		
+    Cli_Cedula varchar(10) unique not null,
+    Cli_Nombre varchar(50) not null,
+    Cli_Apellido varchar(50) not null,
+    Cli_Direccion varchar(30) not null,
+    Cli_Telefono varchar(10) not null,
+    Cli_email varchar(20) not null,
+    Cli_fecha_Nacimiento date not null
     );
 
-create table articulo(
+create table t_articulo(
 	Art_ID int AUTO_INCREMENT primary key,    
-	Art_Nombre varchar(10),
-	Art_Descripcion varchar(45),
-	Art_Precio double,
-	Art_Stock integer
+	Art_Nombre varchar(10) not null,
+	Art_Descripcion varchar(45) not null,
+	Art_Precio double not null,
+	Art_Stock integer not null,
+	Art_Categ varchar(1) not null
     );
     
-create table sucursal(
+create table t_sucursal(
 	Local_ID int AUTO_INCREMENT primary key,
-	Local_Telefono varchar(10),
-	Local_Direccion varchar(45),
-	esMatriz boolean,
-	esSucursal boolean
+	Local_Telefono varchar(10) not null,
+	Local_Direccion varchar(45) not null,
+	esMatriz boolean not null,
+	esSucursal boolean not null
     ); 
 
-create table usuario(
-	Usua_Codigo int AUTO_INCREMENT primary key,
-    Usua_Cedula varchar(10),
-    Usua_Nombre varchar(50),
-    Usua_Direccion varchar(45),
-    Usua_Telefono varchar(15),
-    Local_ID int ,
-    foreign key(Local_ID) references sucursal(Local_ID)
-    );
+CREATE TABLE t_usuario(
+  id_user int NOT NULL AUTO_INCREMENT primary key,
+  nombre varchar(50) NOT NULL,
+  apellido varchar(50) NOT NULL,
+  fecha_nacimiento date NOT NULL,
+  telefono varchar(20) NOT NULL,
+  direccion varchar(50) NOT NULL,
+  correo varchar(50) NOT NULL,
+  usuario varchar(50) NOT NULL,
+  pass varchar(50) NOT NULL,
+  tipo varchar(10) NOT NULL,
+  esSuper bool DEFAULT NULL,
+  Local_ID int not null,
+  cedula varchar(20) NOT NULL,
+  foreign key(Local_ID) references t_sucursal(Local_ID) 
+);
 
-
-create table venta(
+create table t_venta(
 	Venta_ID int AUTO_INCREMENT primary key,
-	Venta_Fecha date,
-    Venta_Hora time,
-    Venta_Subtotal double,
-	Venta_Total double,
-    Cliente_ID int,
-    Usua_ID int,
-    foreign key(Cliente_ID)references cliente(Cli_Cedula),
-    foreign key(Usua_ID)references usuario(Usua_Codigo)
-    );
+	Venta_Fecha date not null,
+    Venta_Hora time not null,
+    Venta_Subtotal double not null,
+	Venta_Total double not null,
+    Cliente_ID int not null,
+    Usua_ID int not null,
+	id_PagoE int,
+	id_PagoC int,
+    foreign key(Cliente_ID)references t_cliente(Cli_id),
+    foreign key(Usua_ID)references t_usuario(id_user),
+    foreign key(id_PagoE)references t_pagoEfectivo(PE_ID),
+    foreign key(id_PagoC)references t_pagoCredito(PC_ID)
+);
 
-create table pagoCredito(
+create table t_pagoCredito(
 	PC_ID int AUTO_INCREMENT primary key,
-    PC_Fecha date,
-    PC_Subtotal float,
-    PC_Impuestos float,
-    PC_PagoCorriente boolean,
-    PC_PagoDiferido boolean,
-    PC_MesesDiferidos int,
-    PC_VentaID int,
-    foreign key(PC_VentaID) references venta(Venta_ID)
-    );
+    PC_Fecha date not null,
+    PC_Subtotal float not null,
+    PC_Impuestos float not null,
+    PC_PagoCorriente boolean not null,
+    PC_PagoDiferido boolean not null,
+    PC_MesesDiferidos int
+);
     
-create table pagoEfectivo(
+create table t_pagoEfectivo(
 	PE_ID int AUTO_INCREMENT primary key,
-    PE_Fecha date,
-    PE_Subtotal float,
-    PE_Impuestos float,
-    PE_VentaID int,
-    foreign key(PE_VentaID) references venta(Venta_ID)
-    );
+    PE_Fecha date not null,
+    PE_Subtotal float not null,
+    PE_Impuestos float not null
+);
     
     
-create table detalle_venta(
+create table t_detalle_venta(
 	Detalle_ID int AUTO_INCREMENT primary key,
-	Venta_ID int,
-    Articulo_ID int,
-    foreign key(Venta_ID)references venta(Venta_ID),
-    foreign key(Articulo_ID)references articulo(Art_ID)
+	Venta_ID int not null,
+    Articulo_ID int not null,
+    foreign key(Venta_ID)references t_venta(Venta_ID),
+    foreign key(Articulo_ID)references t_articulo(Art_ID)
     );
 
-create table cotizacion(
+create table t_cotizacion(
 	Cot_ID int AUTO_INCREMENT primary key,
-    Cot_Fecha date,
-    Cot_Valor double,
-    Cliente_ID int,
-    Usua_Codigo int,
-    foreign key(Cliente_ID)references cliente(Cli_Cedula),
-    foreign key(Usua_Codigo)references usuario(Usua_Codigo)  
+    Cot_Fecha date not null,
+    Cot_Valor double not null,
+    Cliente_ID int not null,
+    Usua_Codigo int not null,
+    foreign key(Cliente_ID)references t_cliente(Cli_id),
+    foreign key(Usua_Codigo)references t_usuario(id_user)  
     );
     
-create table detalleCotizacion(
+create table t_detalleCotizacion(
 	Detalle_ID int AUTO_INCREMENT primary key,
     Cot_ID int,
 	Articulo_ID int,
-	foreign key(Articulo_ID)references articulo(Art_ID),
-    foreign key(Cot_ID)references cotizacion(Cot_ID)
+	foreign key(Articulo_ID)references t_articulo(Art_ID),
+    foreign key(Cot_ID)references t_cotizacion(Cot_ID)
     );
 
     
