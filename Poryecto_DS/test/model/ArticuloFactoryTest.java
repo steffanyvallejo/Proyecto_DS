7cf73e65dd728f4e539623d669c46db6a8ebd871
Eduardo Ortiz H <svallejo@espol.edu.ec>
2018-08-26 11:32:44 -0500

    pruebas unitarias

---
+++ Poryecto_DS/test/model/ArticuloFactoryTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author eduar
+ */
+public class ArticuloFactoryTest {
+    
+    public ArticuloFactoryTest() {
+    }
+    
+    
+    /**
+     * Test of crearArticulo method, of class ArticuloFactory.
+     */
+    @Test
+    public void testCrearArticulo() {
+        System.out.println("crearArticulo");
+        String modelo = "";
+        String marca = "";
+        String categoria = "";
+        String color = "";
+        float precio = 0.0F;
+        int stock = 0;
+        ArticuloFactory instance = new ArticuloFactory();
+        Articulo result = instance.crearArticulo(modelo, marca, categoria, color, precio, stock);
+        assertNull(result);
+    }
+    
+}
--- Poryecto_DS/test/model/CotizacionTest.java
+++ Poryecto_DS/test/model/CotizacionTest.java
@@ -5,12 +5,7 @@
  */
 package model;
 
-import java.util.Date;
 import java.util.LinkedList;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -21,23 +16,6 @@ public class CotizacionTest {
     
     public CotizacionTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     
 
     /**
