12d74160c8b5ab29f4f75b015049bbdcb0daf343
emiliomoran <emiliojmp9@gmail.com>
2018-08-26 01:34:20 -0500

    Refactoring CargasDB e ReportesDB

--- Poryecto_DS/src/controller/CargasDB.java
+++ Poryecto_DS/src/controller/CargasDB.java
@@ -10,6 +10,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.Administrador;
 import model.Articulo;
 import model.CocinaInduccion;
@@ -24,15 +26,14 @@ import model.Vendedor;
  * @author EmilioMP
  */
 public class CargasDB {
-    
+
     private Connection conexion;
-    
-    
-    public CargasDB(){        
+
+    public CargasDB() {
         this.conexion = new ConexionDBM().establecerConexion(conexion);
     }
-    
-    public Empleado login(String user, String pass) {        
+
+    public Empleado login(String user, String pass) {
         try {
             PreparedStatement pst = conexion.prepareStatement("SELECT * FROM t_usuario WHERE usuario = ? AND pass = ?");
             pst.setString(1, user);
@@ -42,51 +43,18 @@ public class CargasDB {
                 String t = resultados.getString("tipo");
                 if (t.equals("v")) {
                     Vendedor v = new Vendedor();
-                    v.setNombre(resultados.getString("nombre"));
-                    v.setApellido(resultados.getString("apellido"));
-                    v.setFechaNacimiento(resultados.getDate("fecha_nacimiento"));
-                    v.setNumeroTelefonico(resultados.getString("telefono"));
-                    v.setDirecciónDomicilio(resultados.getString("direccion"));
-                    v.setCorreoElectronico(resultados.getString("correo"));
-                    v.setUsuario(resultados.getString("usuario"));
-                    v.setContraseña(resultados.getString("pass"));
-                    v.setTipo(resultados.getString("tipo"));
-                    v.setIsSuper(resultados.getBoolean("esSuper"));
-                    v.setCedula(resultados.getString("cedula"));
-                    pst.close();
-                    resultados.close();
+                    setearDatos(v, resultados);
+                    close(pst, resultados);
                     return v;
                 } else if (t.equals("g")) {
                     Gerente g = new Gerente();
-                    g.setNombre(resultados.getString("nombre"));
-                    g.setApellido(resultados.getString("apellido"));
-                    g.setFechaNacimiento(resultados.getDate("fecha_nacimiento"));
-                    g.setNumeroTelefonico(resultados.getString("telefono"));
-                    g.setDirecciónDomicilio(resultados.getString("direccion"));
-                    g.setCorreoElectronico(resultados.getString("correo"));
-                    g.setUsuario(resultados.getString("usuario"));
-                    g.setContraseña(resultados.getString("pass"));
-                    g.setTipo(resultados.getString("tipo"));
-                    g.setIsSuper(resultados.getBoolean("esSuper"));
-                    g.setCedula(resultados.getString("cedula"));
-                    pst.close();
-                    resultados.close();
+                    setearDatos(g, resultados);
+                    close(pst, resultados);
                     return g;
                 } else if (t.equals("a")) {
                     Administrador a = new Administrador();
-                    a.setNombre(resultados.getString("nombre"));
-                    a.setApellido(resultados.getString("apellido"));
-                    a.setFechaNacimiento(resultados.getDate("fecha_nacimiento"));
-                    a.setNumeroTelefonico(resultados.getString("telefono"));
-                    a.setDirecciónDomicilio(resultados.getString("direccion"));
-                    a.setCorreoElectronico(resultados.getString("correo"));
-                    a.setUsuario(resultados.getString("usuario"));
-                    a.setContraseña(resultados.getString("pass"));
-                    a.setTipo(resultados.getString("tipo"));
-                    a.setIsSuper(resultados.getBoolean("esSuper"));
-                    a.setCedula(resultados.getString("cedula"));
-                    pst.close();
-                    resultados.close();
+                    setearDatos(a, resultados);
+                    close(pst, resultados);
                     return a;
                 }
             }
@@ -97,27 +65,54 @@ public class CargasDB {
         return null;
     }
 
-    public LinkedList<Articulo> cargarArticulos(){
-        LinkedList<Articulo> articulosDisponibles = new LinkedList<Articulo>();        
+    private void setearDatos(Empleado emp, ResultSet resultados) {
+        try {
+            emp.setNombre(resultados.getString("nombre"));
+            emp.setApellido(resultados.getString("apellido"));
+            emp.setFechaNacimiento(resultados.getDate("fecha_nacimiento"));
+            emp.setNumeroTelefonico(resultados.getString("telefono"));
+            emp.setDirecciónDomicilio(resultados.getString("direccion"));
+            emp.setCorreoElectronico(resultados.getString("correo"));
+            emp.setUsuario(resultados.getString("usuario"));
+            emp.setContraseña(resultados.getString("pass"));
+            emp.setTipo(resultados.getString("tipo"));
+            emp.setIsSuper(resultados.getBoolean("esSuper"));
+            emp.setCedula(resultados.getString("cedula"));
+        } catch (SQLException ex) {
+            Logger.getLogger(CargasDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void close(PreparedStatement pst, ResultSet resultados) {
+        try {
+            pst.close();
+            resultados.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(CargasDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public LinkedList<Articulo> cargarArticulos() {
+        LinkedList<Articulo> articulosDisponibles = new LinkedList<Articulo>();
         System.out.println("conecta db para buscar c");
         try {
             System.out.println("entra al nombre");
-            PreparedStatement pst = conexion.prepareStatement("SELECT * FROM t_articulo");           
+            PreparedStatement pst = conexion.prepareStatement("SELECT * FROM t_articulo");
             ResultSet rs = pst.executeQuery();
             System.out.println("ejecucion del query");
             while (rs.next()) {
-                if(rs.getString("Art_Categ").equals("cocina")){
-                    CocinaInduccion cocina = new CocinaInduccion(rs.getString("Art_Modelo"), rs.getString("Art_Marca"), rs.getString("Art_Categ"), rs.getString("Art_Color"), rs.getFloat("Art_Precio"), rs.getInt("Art_Stock"));                    
+                if (rs.getString("Art_Categ").equals("cocina")) {
+                    CocinaInduccion cocina = new CocinaInduccion(rs.getString("Art_Modelo"), rs.getString("Art_Marca"), rs.getString("Art_Categ"), rs.getString("Art_Color"), rs.getFloat("Art_Precio"), rs.getInt("Art_Stock"));
                     articulosDisponibles.add(cocina);
-                    
-                }else if(rs.getString("Art_Categ").equals("lavadora")){
+
+                } else if (rs.getString("Art_Categ").equals("lavadora")) {
                     Lavadora lavadora = new Lavadora(rs.getString("Art_Modelo"), rs.getString("Art_Marca"), rs.getString("Art_Categ"), rs.getString("Art_Color"), rs.getFloat("Art_Precio"), rs.getInt("Art_Stock"));
                     articulosDisponibles.add(lavadora);
-                    
-                }else if(rs.getString("Art_Categ").equals("refrigeradora")){
+
+                } else if (rs.getString("Art_Categ").equals("refrigeradora")) {
                     Refrigeradora refrigeradora = new Refrigeradora(rs.getString("Art_Modelo"), rs.getString("Art_Marca"), rs.getString("Art_Categ"), rs.getString("Art_Color"), rs.getFloat("Art_Precio"), rs.getInt("Art_Stock"));
                     articulosDisponibles.add(refrigeradora);
-                }                                
+                }
             }
             return articulosDisponibles;
         } catch (SQLException ex) {
--- Poryecto_DS/src/controller/ReportesDB.java
+++ Poryecto_DS/src/controller/ReportesDB.java
@@ -13,77 +13,62 @@ import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author EmilioMP
  */
 public class ReportesDB {
-    
+
     private Connection conexion;
-    
-    
-    public ReportesDB(){
-        this.conexion=new ConexionDBM().establecerConexion(conexion);
+    LinkedList<LinkedList<String>> datosRep;
+    LinkedList<String> fechas;
+
+    public ReportesDB() {
+        this.conexion = new ConexionDBM().establecerConexion(conexion);
+        fechas();
     }
-    
-    public LinkedList<LinkedList<String>> reporteVendedor() {        
-        Calendar c = Calendar.getInstance();
-        SimpleDateFormat formateador = new SimpleDateFormat("yyyy-MM-dd");
-        c.add(Calendar.DATE, -7);
-        java.util.Date pasado = c.getTime();
-        java.util.Date ahora = new java.util.Date();
-        String pasadoS = formateador.format(pasado);
-        String ahoraS = formateador.format(ahora);
-        
-        LinkedList<LinkedList<String>> datos = new LinkedList<LinkedList<String>>();
-        
+
+    public LinkedList<LinkedList<String>> reporteVendedor() {
+        datosRep = new LinkedList<LinkedList<String>>();
         try {
             System.out.println("entra al nombre");
-            PreparedStatement pst = conexion.prepareStatement("select v.Usua_ID as id, u.nombre as nombre, count(*) as cantidad, sum(v.Venta_Total) as total from t_usuario u, t_venta v where v.Usua_ID = u.id_user and v.Venta_Fecha between ? and ? group by v.Usua_ID;");        
-            pst.setDate(1, Date.valueOf(pasadoS));
-            pst.setDate(2, Date.valueOf(ahoraS));
+            PreparedStatement pst = conexion.prepareStatement("select v.Usua_ID as id, u.nombre as nombre, count(*) as cantidad, sum(v.Venta_Total) as total from t_usuario u, t_venta v where v.Usua_ID = u.id_user and v.Venta_Fecha between ? and ? group by v.Usua_ID;");
+            pst.setDate(1, Date.valueOf(fechas.get(0)));
+            pst.setDate(2, Date.valueOf(fechas.get(1)));
             ResultSet rs = pst.executeQuery();
-            System.out.println("ejecucion del query");
-            while (rs.next()) {
-                LinkedList<String> temp = new LinkedList<String>();
-                String id = Integer.toString(rs.getInt("id"));
-                String nombre = rs.getString("nombre");
-                String cantidad = Integer.toString(rs.getInt("cantidad"));
-                String total = Float.toString(rs.getFloat("total"));
-                temp.add(id);
-                temp.add(nombre);
-                temp.add(cantidad);
-                temp.add(total);
-                datos.add(temp);
-            }
-            return datos;
+            System.out.println("ejecucion del query");           
+            guardarDatosRep(rs, datosRep);
+            return datosRep;
         } catch (SQLException ex) {
             System.out.println("Error al no encontrar el producto en la db");
             System.out.println(ex);
         }
-       
         return null;
     }
-    
-    public LinkedList<LinkedList<String>> reporteArticulo() {        
-        Calendar c = Calendar.getInstance();
-        SimpleDateFormat formateador = new SimpleDateFormat("yyyy-MM-dd");
-        c.add(Calendar.DATE, -7);
-        java.util.Date pasado = c.getTime();
-        java.util.Date ahora = new java.util.Date();
-        String pasadoS = formateador.format(pasado);
-        String ahoraS = formateador.format(ahora);
-                
-        LinkedList<LinkedList<String>> datos = new LinkedList<LinkedList<String>>();
-        
+
+    public LinkedList<LinkedList<String>> reporteArticulo() {       
+        datosRep = new LinkedList<LinkedList<String>>();
         try {
             System.out.println("entra al nombre");
-            PreparedStatement pst = conexion.prepareStatement("select dv.Articulo_ID as id, a.Art_Modelo as nombre, count(*) as cantidad, sum(v.Venta_Total) as total from t_articulo a, t_venta v, t_detalle_venta dv where dv.Venta_ID = v.Venta_ID and dv.Articulo_ID = a.Art_ID and v.Venta_Fecha between ? and ? group by dv.Articulo_ID;");        
-            pst.setDate(1, Date.valueOf(pasadoS));
-            pst.setDate(2, Date.valueOf(ahoraS));
+            PreparedStatement pst = conexion.prepareStatement("select dv.Articulo_ID as id, a.Art_Modelo as nombre, count(*) as cantidad, sum(v.Venta_Total) as total from t_articulo a, t_venta v, t_detalle_venta dv where dv.Venta_ID = v.Venta_ID and dv.Articulo_ID = a.Art_ID and v.Venta_Fecha between ? and ? group by dv.Articulo_ID;");
+            pst.setDate(1, Date.valueOf(fechas.get(0)));
+            pst.setDate(2, Date.valueOf(fechas.get(1)));
             ResultSet rs = pst.executeQuery();
-            System.out.println("ejecucion del query");
+            System.out.println("ejecucion del query");            
+            guardarDatosRep(rs, datosRep);
+            return datosRep;
+        } catch (SQLException ex) {
+            System.out.println("Error al no encontrar el producto en la db");
+            System.out.println(ex);
+        }
+        return null;
+    }
+
+    private void guardarDatosRep(ResultSet rs, LinkedList<LinkedList<String>> datosRep) {
+        try {
             while (rs.next()) {
                 LinkedList<String> temp = new LinkedList<String>();
                 String id = Integer.toString(rs.getInt("id"));
@@ -94,13 +79,21 @@ public class ReportesDB {
                 temp.add(nombre);
                 temp.add(cantidad);
                 temp.add(total);
-                datos.add(temp);
+                datosRep.add(temp);
             }
-            return datos;
         } catch (SQLException ex) {
-            System.out.println("Error al no encontrar el producto en la db");
-            System.out.println(ex);
-        }       
-        return null;
+            Logger.getLogger(ReportesDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void fechas(){
+        this.fechas=new LinkedList<>();
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat formateador = new SimpleDateFormat("yyyy-MM-dd");
+        c.add(Calendar.DATE, -7);
+        java.util.Date pasado = c.getTime();
+        java.util.Date ahora = new java.util.Date();
+        fechas.add(formateador.format(pasado));
+        fechas.add(formateador.format(ahora));                
     }
 }
